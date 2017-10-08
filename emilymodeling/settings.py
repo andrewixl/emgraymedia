f8cebef44d222ae5881be9e56bf346e8c0c1a018
Andrew Burger <burger.andrewixl@gmail.com>
2017-10-07 20:29:09 -0700

    Portfolio Fix

--- emilymodeling/settings.py
+++ emilymodeling/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['emgraymedia.gq', 'www.emgraymedia.gq', '52.27.250.145']
+ALLOWED_HOSTS = ['emgraymedia.gq', 'www.emgraymedia.gq', '52.27.250.145', 'localhost']
 
 
 # Application definition
